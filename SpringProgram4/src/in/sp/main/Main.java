bbcfebab314552582fe352d5ebe917d62851336f
Suvam-Debnath <125594809+Suvam-Debnath@users.noreply.github.com>
2024-08-18 20:49:58 +0530

    Add files via upload

---
+++ SpringProgram4/src/in/sp/main/Main.java
@@ -0,0 +1,19 @@
+package in.sp.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.beans.Student;
+import in.sp.resources.SpringConfigFile;
+public class Main {
+	public static void main(String[] srgs) {
+		
+		String resource_file_path = "/in/sp/resources/applicatoinContext.xml";
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
+		
+		Student std = (Student)context.getBean("student");
+		std.display();
+	}
+
+}
